734aca98556b048b7a039fe4eb78205bcc3ff336
sujeong23 <tnwjd10041004@hanmail.net>
2020-01-28 11:43:50 +0900

    create to ex3.js

---
+++ ex3.js
@@ -0,0 +1,12 @@
+let level = "A";
+
+switch(level){
+    case "A" :
+        console.log("VIP LEVEL~!");
+        break;
+    case "B" :
+        console.log("Basic LEVEL!");
+        break;
+    default : 
+        console.log("nothing level");
+}
